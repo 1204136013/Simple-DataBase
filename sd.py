19fa05d79ecc577f4283f84c41fe39c65e4728b9
fanlong <fanlong@liquidnetwork.com>
2022-06-13 21:01:46 +0800

    分离 meta_command 和 准备解析常规语句

--- sd.py
+++ sd.py
@@ -3,13 +3,36 @@ def main():
         # print_prompt()
         input_buffer = input('db:>>')
         # read_input(inp`ut_buffer)
-        if input_buffer == '.exit':
-            # close_input_buffer(input_buffer)
-            exit('exit')
-        elif input_buffer.isspace() or len(input_buffer) == 0:
+        if input_buffer.startswith('.'):
+            do_meta_command(input_buffer)
+        elif donothing(input_buffer):
             continue
         else:
-            print("Unrecognized command {} \n".format(input_buffer))
+            statement = prepare_statement(input_buffer)
+            execute_statement(statement)
+
+
+def prepare_statement(command):
+    if True:
+        return command
+    else:
+        print("Unrecognized command {} \n".format(command))
+        return None
+
+
+def execute_statement(statement):
+    print('executed')
+
+
+def donothing(command):
+    return command.isspace() or len(command) == 0
+
+
+def do_meta_command(command):
+    if command == '.exit':
+        exit('exit')
+    else:
+        print("Unrecognized command {} \n".format(command))
 
 
 if __name__ == '__main__':
