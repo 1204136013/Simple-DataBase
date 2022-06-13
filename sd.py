9efb54e69a79663f50146b3a550d0e74a05b8760
fanlong <fanlong@liquidnetwork.com>
2022-06-13 20:45:25 +0800

    基础架子

--- sd.py
+++ sd.py
@@ -1,11 +1,13 @@
 def main():
     while True:
         # print_prompt()
-        input_buffer = input(':>>')
-        # read_input(input_buffer)
+        input_buffer = input('db:>>')
+        # read_input(inp`ut_buffer)
         if input_buffer == '.exit':
             # close_input_buffer(input_buffer)
             exit('exit')
+        elif input_buffer.isspace() or len(input_buffer) == 0:
+            continue
         else:
             print("Unrecognized command {} \n".format(input_buffer))
 
